8d2d03b5bdccc4ce0ff53d6a71a908132726c1c0
HJK <danyil.butov@gmail.com>
2024-12-10 14:28:48 +0000

    feat: add types

---
+++ back-end/api/type/file.go
@@ -0,0 +1,16 @@
+package _type
+
+type FileStore interface {
+	CreateFile(payload *CreateFilePayload) (*File, error)
+}
+
+type File struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+type CreateFilePayload struct {
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
---
+++ back-end/api/type/history.go
@@ -0,0 +1,22 @@
+package _type
+
+import "time"
+
+type HistoryStore interface {
+	GetHistory() ([]*History, error)
+	CreateHistory(payload *CreateHistoryPayload) (*History, error)
+}
+
+type History struct {
+	ID             string    `json:"id"`
+	Author         string    `json:"author"`
+	Description    string    `json:"description"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	UpdatedFileIDs []string  `json:"updated_files"`
+}
+
+type CreateHistoryPayload struct {
+	Author         string   `json:"author"`
+	Description    string   `json:"description"`
+	UpdatedFileIDs []string `json:"updated_files"`
+}
